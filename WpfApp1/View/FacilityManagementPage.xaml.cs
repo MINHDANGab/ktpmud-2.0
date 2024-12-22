4e106f6df9b7cf72fc99ac67bfc62d8c240330d2
MINHDANGab <thaotrau55@gmail.com>
2024-12-22 22:23:16 +0700

    Initial commit

---
+++ WpfApp1/View/FacilityManagementPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Controls;
+using WpfApp1.ViewModel;
+using WpfApp1.Model;
+
+namespace WpfApp1.View
+{
+    public partial class FacilityManagementPage : Page
+    {
+        public FacilityManagementPage()
+        {
+            InitializeComponent();
+
+            // Tạo một đối tượng Coso_Service
+            var cosoService = new Coso_Service(new YourDbContext());
+
+            // Truyền Coso_Service vào ViewModel
+            DataContext = new FacilityManagementViewModel(cosoService);
+        }
+    }
+}
